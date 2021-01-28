b38778f72e7ec450839a0f20a774625565275f1e
Wurdsmith <wurdsmithing@gmail.com>
2021-01-27 21:46:24 -0600

    commit tree efc5347b211536ac545038fe6e82bbea068b3cbf

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Backer
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        project_backers = ProjectBacker.all.select do |project|
+            project.backer == self
+        end
+        project_backers.collect do |pb|
+            pb.project
+        end
+        
+    end
+end
